9249d8ebce88135c8f25bef218039122e29f9837
OctavioBenjamin <benjaminmen55@gmail.com>
2022-09-01 20:36:59 -0300

    Nuevas funciones punto 3 - 6

--- main.py
+++ main.py
@@ -33,11 +33,96 @@ def main():
             if listaCreada == False:
                 print("ERROR: No se han encontrado proyectos cargados")
             else:
-                ordenamiento_ss(proyectos)
+                ordenamiento_ss_alf(proyectos)
                 for i in range(len(proyectos)):
                     print(f"Proyecto {proyectos[i].nombre} Numero: {proyectos[i].numero}")
                 
             opcion = -1
+        
+        elif opcion == 3:
+            seguir = 1
+            while seguir == 1:
+                num_filtro = int(input("Ingrese el numero de proyecto para actualizar: "))
+                ordenamiento_ss_num(proyectos)
+                
+                if binary_search(proyectos, num_filtro) == -1:
+                    print(f"¡Error! No se ha encontrado ningun proyecto con el numero {filtro}")
+                else:
+                    nuevas_lineas = int(input("Cantidad de lineas a actualizar: "))
+                    proyectos[binary_search(proyectos, num_filtro)].lineas = nuevas_lineas
+                    nuevo_dd = int(input("Ingrese dia de actulización:  "))
+                    nuevo_mm = int(input("Ingrese mes de actualizacion: "))
+                    nuevo_yy = int(input("Ingrese año de actualización: "))
+                    proyectos[binary_search(proyectos, num_filtro)].actualizacion = str(f"{nuevo_dd}-{nuevo_mm}-{nuevo_yy}")
+                
+                seguir = int(input("Si desea seguir actualizando seleccione 1.\nSi desea volver al menu seleccione 0.\n "))
+                while 1 < seguir < 0:
+                    print("¡Error! Debe de seleccionar una opcion valida.")
+                    seguir = int(input("Si desea seguir actualizando seleccione 1.\nSi desea volver al menu seleccione 0.\n "))
+
+        elif opcion == 4:
+            # Cantidad de lineas por lenguaje
+            len_filtro = int(input("Ingrese el lenguaje que desea buscar: "))
+            cant_lineas = 0
+            resultados = []
+            for i in range(len(proyectos)):
+                if proyectos[i].lenguaje ==  len_filtro:
+                    cant_lineas += proyectos[i].lineas
+                    resultados.append(proyectos[i])
+            
+            if len(resultados) > 0:
+                print(f"Se encontraron {len(resultados)} proyectos en {lenguajes[len_filtro]}")
+                for i in range(len(resultados)):
+                    ordenamiento_ss_num(resultados)
+                    print(f"{resultados[i].nombre}: Proyecto numero {resultados[i].numero} con {resultados[i].lineas} lineas codigo")    
+            
+            else:
+                print("¡ERROR! No se encontraron resultados")
+
+        elif opcion == 5:
+            if len(proyectos) == 0:
+                print("¡ERROR! No se han encontrado proyectos cargados")
+            else:
+                años_conteo = 22*[0]
+                # Calcular la cantida de proyectos por año de actualizacion
+                # Mostrar los años que solo tengan algun proyecto
+                año_pivot = None
+                
+                for i in range(22):
+                    año_pivot = str(proyectos[i].año)
+                    año_indice = int(año_pivot[-2]+año_pivot[-1])
+                    años_conteo[año_indice] += 1
+                
+                for i in range(22):
+                    if años_conteo[i] > 0:
+                        print(f"Se encontraron {años_conteo[i]} proyecto/s en {años[i]}")
+                    
+        elif opcion == 6:
+            #Filtrar por lenguajes ordenados alfabeticamente
+            if len(proyectos) == 0:
+                print("¡ERROR! No se han encontrado proyectos cargados")
+            else:
+                len_filtro = int(input("Ingrese el lenguaje que desea buscar: "))
+                proyectos_busqueda = []
+
+                for i in range(len(proyectos)):
+                    if proyectos[i].lenguaje == len_filtro:
+                        proyectos_busqueda.append(proyectos[i])
+            
+            if len(proyectos_busqueda) == 0:
+                print(f"No se han encontrado proyectos con {lenguajes[len_filtro]}")
+            else:
+                ordenamiento_ss_alf(proyectos_busqueda)
+                print(f"Se encontraron {len(proyectos_busqueda)} proyectos.")
+                for i in range(len(proyectos_busqueda)):
+                    print(f"Proyecto {proyectos_busqueda[i].titulo} Numero {proyectos_busqueda[i].numero}")
+
+
+                
+            
+
+
+            
 
 if __name__ == "__main__":
     main()
--- modulo2.py
+++ modulo2.py
@@ -3,12 +3,16 @@ from main import *
 
 class Proyecto:
 
-    def __init__(self, num, tit, actualizacion, lenguaje,lineas):
+    def __init__(self, num, tit, año, actualizacion, lenguaje,lineas):
         self.numero = num
         self.titulo = tit
+        self.año = año
         self.actualizacion = actualizacion
         self.lenguaje = lenguaje
         self.lineas = lineas
+
+lenguajes = ["Python", "Java", "C++", "Javascript", "Shell", "HTML", "Ruby", "Swift", "C#", "VB", "Go"]
+años = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
 
 def cargar_proyecto():
     num = int(input("Indicar numero de proyecto: "))
@@ -19,9 +23,10 @@ def cargar_proyecto():
     fecha_actu = str(f"{dd_actu}-{mm_actu}-{yy_actu}")
     lenguaje = int(input("0:  Python  -   1:  Java\n2:  C++    -   3:  Javascript\n4:  Shell   -   5:  HTML\n6:  Ruby    -   7:  Swift\n8:  C#      -   9:  VB\n10: Go\nIngrese el lenguaje utilizado: "))
     lineas = int(input("Ingrese la cantidad de lineas de codigo: "))
-    nuevo_proyecto = Proyecto(num, tit, fecha_actu, lenguaje, lineas)
+    nuevo_proyecto = Proyecto(num, tit, yy_actu, fecha_actu, lenguaje, lineas)
     print("¡PROYECTO CARGADO!\n")
     return nuevo_proyecto
+
 
 def menu():
     print("""
@@ -44,7 +49,7 @@ def selec_op():
         op = int(input("Seleccione una opcion valida: "))
     return op
 
-def ordenamiento_ss(proyectos):
+def ordenamiento_ss_num(proyectos):
     long= len(proyectos)
 
     for i in range(long-1):
@@ -52,3 +57,28 @@ def ordenamiento_ss(proyectos):
             if proyectos[i].numero > proyectos[j].numero:
                 # Intercambiar
                 proyectos[i].numero, proyectos[j].numero = proyectos[j].numero, proyectos[i].numero
+                
+def ordenamiento_ss_alf(proyectos):
+    long= len(proyectos)
+
+    for i in range(long-1):
+        for j in range(i+1, long):
+            if proyectos[i].titulo > proyectos[j].titulo:
+                # Intercambiar
+                proyectos[i].titulo, proyectos[j].titulo = proyectos[j].titulo, proyectos[i].titulo
+
+def binary_search(v, x):
+	# busqueda binaria... asume arreglo ordenado...
+	izq, der = 0, len(v) - 1
+	while izq <= der:
+		mid = (izq + der) // 2
+		if x == v[mid]:
+			return mid
+
+		elif x < v[mid]:
+			der = mid - 1
+
+		else:
+			izq = mid + 1
+	return -1
+
